5296c14bb188bfafe98eeb91490de6ee3fe3ff0d
jalilou <41523564+jalilou@users.noreply.github.com>
2018-07-21 22:10:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,93 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+
+
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:wave:  Welcome to TBA :wave: 
+:crown:  ${member} :crown:  
+Rules:
+1.    Respect everyone, no politics and religious discussion
+2.    Behave as a professional player
+3.    Make sure you connect on discord first and join the voice channel
+4.    It is only allowable to start squad battle when Leader, Co-Leader or Elder is  available
+5.    When you’re -3 you exit
+6.    If you’re using hacks, it is preferably to kick yourself 
+7.    Stay active for squad battle
+8.    Act as a team player and loyal as well, cause you might exit from the match sometimes even if you’re not negative
+9.    If you’re not willing to be a loyal player, then kick yourself again
+u'are member  ${member.guild.memberCount}`) 
+}).catch(console.error)
+})
+client.on('message', message => {
+if(message.content.startsWith('hi')) {
+    message.reply("Hello!");
+}
+if(message.content.startsWith('Hi')) {
+    message.reply("Hello!");
+}
+if(message.content.startsWith('hello')) {
+    message.reply("Hello!");
+}
+if(message.content.startsWith('Hello')) {
+    message.reply("Hello!");
+}
+if(message.content.startsWith('gn')) {
+    message.reply("Good Night Bro!");
+}
+if(message.content.startsWith('Gn')) {
+    message.reply("Good Night Bro!");
+}
+if(message.content.startsWith('good night')) {
+    message.reply("Good Night Bro!");
+}
+if(message.content.startsWith('Good night')) {
+    message.reply("Good Night Bro!");
+}
+if(message.content.startsWith('puta')) {
+    message.reply("ur the only puta here!");
+}
+if(message.content.startsWith('Puta')) {
+    message.reply("ur the only puta here!");
+}
+if(message.content.startsWith('gay')) {
+    message.reply("ur the only gay here!");
+}
+if(message.content.startsWith('Gay')) {
+    message.reply("ur the only gay here!");
+}
+if(message.content.startsWith('sb')) {
+    message.reply("@everyone +2");
+}
+if(message.content.startsWith('Sb')) {
+    message.reply("@everyone +2");
+}
+if(message.content.startsWith('.')) {
+    message.reply("...");
+}
+if(message.content.startsWith('good morning')) {
+    message.reply("Good Morning Bro!");
+}
+if(message.content.startsWith('Good morning')) {
+    message.reply("Good Morning Bro!");
+}
+if(message.content.startsWith('cya')) {
+    message.reply("Cya Bro!");
+}
+if(message.content.startsWith('Cya')) {
+    message.reply("Cya Bro!");
+}
+
+});
+
+
+client.on('ready', () => {
+  client.user.setGame('Jalil','https://www.twitch.tv/v5bz');
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
